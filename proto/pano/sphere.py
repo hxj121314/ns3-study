f51676e1ced7477439e5799c97c463378857b15e
张小奕 <zxyqwe_2004@126.com>
2017-12-11 16:34:52 -0500

    v2

--- proto/pano/sphere.py
+++ proto/pano/sphere.py
@@ -5,78 +5,80 @@ from PIL import Image
 import numpy
 
 
-def run_scene():
-    glutInit()
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-    glutInitWindowSize(300, 300)
-    glutCreateWindow("Sphere")
-    glutHideWindow()
+class Sphere(object):
+    def __init__(self, size, f=None):
+        self._w, self._h = size
+        glutInit()
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+        glutInitWindowSize(self._w, self._h)
+        glutCreateWindow("Sphere")
+        glutHideWindow()
 
-    glClearColor(0., 0., 0., 0.)
-    glClearDepth(1.)
-    glDepthFunc(GL_LESS)
-    glEnable(GL_DEPTH_TEST)
+        glClearColor(0., 0., 0., 0.)
+        glClearDepth(1.)
+        glDepthFunc(GL_LESS)
+        glEnable(GL_DEPTH_TEST)
 
-    glShadeModel(GL_SMOOTH)
-    glMatrixMode(GL_PROJECTION)
-    gluPerspective(40, 1, 1, 500)
-    glMatrixMode(GL_MODELVIEW)
-    gluLookAt(0, 0, 0,
-              0, 0, 1,
-              0, 1, 0)
-    glPushMatrix()
+        glShadeModel(GL_SMOOTH)
+        glMatrixMode(GL_PROJECTION)
+        gluPerspective(40, self._w * 1.0 / self._h, 1.0, 500.0)
+        glMatrixMode(GL_MODELVIEW)
+        gluLookAt(0, 0, 0,
+                  0, 0, 1,
+                  0, 1, 0)
+        glPushMatrix()
 
-    draw_sphere()
+        if f is None:
+            f = os.path.dirname(os.path.realpath(__file__)) + '/pano.jpg'
+        self._read_texture(f)
 
+    def draw_sphere(self, *args):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glPushMatrix()
 
-def draw_sphere():
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glPushMatrix()
-    texture_id = read_texture()
+        glTranslatef(0., 0., 0.)
+        glScalef(-1, 1, 1)
 
-    glTranslatef(0., 0., 0.)
-    glScalef(-1, 1, 1)
+        x, y, z = args
+        glRotatef(x, 1., 0., 0.)
+        glRotatef(y, 0., 1., 0.)
+        glRotatef(z, 0., 0., 1.)
 
-    x, y, z = (0, 0, 0)
-    glRotatef(x, 1., 0., 0.)
-    glRotatef(y, 0., 1., 0.)
-    glRotatef(z, 0., 0., 1.)
+        glEnable(GL_TEXTURE_2D)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
 
-    glEnable(GL_TEXTURE_2D)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+        glBindTexture(GL_TEXTURE_2D, self._texture_id)
 
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
-    glBindTexture(GL_TEXTURE_2D, texture_id)
+        glEnable(GL_TEXTURE_GEN_S)
+        glEnable(GL_TEXTURE_GEN_T)
+        glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP)
+        glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP)
+        glutSolidSphere(18, 200, 100)
+        glDisable(GL_TEXTURE_2D)
 
-    glEnable(GL_TEXTURE_GEN_S)
-    glEnable(GL_TEXTURE_GEN_T)
-    glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP)
-    glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_SPHERE_MAP)
-    glutSolidSphere(18, 200, 100)
-    glDisable(GL_TEXTURE_2D)
+        buf = glReadPixels(0, 0, self._w, self._h, GL_RGB, GL_UNSIGNED_BYTE)
+        image = Image.frombytes(mode="RGB", size=(self._w, self._h), data=buf)
+        image.show()
 
-    buf = glReadPixels(0, 0, 300, 300, GL_RGB, GL_UNSIGNED_BYTE)
-    image = Image.frombytes(mode="RGB", size=(300, 300), data=buf)
-    image.show()
+        glPopMatrix()
+        glutSwapBuffers()
 
-    glPopMatrix()
-    glutSwapBuffers()
+    def _read_texture(self, filename):
+        img = Image.open(filename)
+        img_data = numpy.array(list(img.getdata()), numpy.int8)
+        texture_id = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, texture_id)
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
 
-
-def read_texture(filename=os.path.dirname(os.path.realpath(__file__)) + '/pano.jpg'):
-    img = Image.open(filename)
-    img_data = numpy.array(list(img.getdata()), numpy.int8)
-    texture_id = glGenTextures(1)
-    glBindTexture(GL_TEXTURE_2D, texture_id)
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.size[0], img.size[1], 0,
-                 GL_RGB, GL_UNSIGNED_BYTE, img_data)
-    return texture_id
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.size[0], img.size[1], 0,
+                     GL_RGB, GL_UNSIGNED_BYTE, img_data)
+        self._texture_id = texture_id
 
 
 if __name__ == '__main__':
-    run_scene()
+    s = Sphere((600, 300))
+    s.draw_sphere()
